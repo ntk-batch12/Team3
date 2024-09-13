a8158863cd76fafc5c3c71d3651ec8839b410660
hamsa <hamsa@Hamsa.mynetworksettings.com>
2024-09-12 21:04:43 -0400

    changes

--- src/abc/Team3Demo.java
+++ src/abc/Team3Demo.java
@@ -2,6 +2,10 @@ package abc;
 
 public class Team3Demo {
 	public static void main(String[] args) {
+		
+		String n1 = "john";
+		
+		
 		System.out.println("Team 3...");
 	
 	System.out.println("Are we going to work yet");

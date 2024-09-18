69c6ea1d6f33aa1cbeeb0ae7f397edc41b7b6b9f
RazEbrat <raz.tekschool@gmail.com>
2024-09-17 20:38:57 -0400

    last commit

--- src/abc/Team3Demo.java
+++ src/abc/Team3Demo.java
@@ -2,20 +2,20 @@ package abc;
 
 public class Team3Demo {
 	public static void main(String[] args) {
+
+		String n1 = "john";
+		int i = 24;
+
+		System.out.println(n1 + i);
+
 		System.out.println("Team 3...");
 
 		System.out.println("Are we going to work yet");
-		
-		String n1 = "john";
-		int i =24;
-		
-		System.out.println(n1 + i);		
-		
-		System.out.println("Team 3...");
-	
-	System.out.println("Are we going to work yet");
 
 	}
+
+	String name = "john";
+	int num = 24;
 
 	public static int addNums(int a, int b) {
 		int result = a + b;
@@ -23,21 +23,16 @@ public class Team3Demo {
 		return result;
 	}
 
-
 	public static int addNums(int a, int b, int c) {
 		int result = a + b + c;
 
 		return result;
 	}
+
 	public static String toLower(String name) {
 		String result = name.toLowerCase();
 
 		return result;
-
-		System.out.println ("push succesful, changed to stage");
-
-		
-
-		return result;
 	}
+
 }
